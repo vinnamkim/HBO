9795bc511cf6c6b91a8677bc5148e7fcc24b7911
VinnamKim <vinnamkim@kaist.ac.kr>
2017-05-12 14:01:35 +0900

    update

--- SIBO.py
+++ SIBO.py
@@ -22,15 +22,15 @@ def Sample_unitball(N, D, epsilon):
     return X * (1. - epsilon)
 
 class SIBO:
-    def __init__(self, fun, K, m_X, m_Phi, ACQ_FUN, SEARCH_METHOD, iter_fit):
+    def __init__(self, fun, K, m_X, m_Phi, epsilon, C2, iter_fit):
         D = fun.D
         
         data = {}
         data['A'] = np.eye(K, dtype = settings.dtype)
         data['b'] = np.sqrt(D) * np.ones(shape = [K, 1], dtype = settings.dtype)
         
-        epsilon = settings.SIBO_epsilon
-        C2 = settings.SIBO_C2
+        #epsilon = settings.SIBO_epsilon
+        #C2 = settings.SIBO_C2
         
         X_center = Sample_unitball(m_X, D, epsilon)
         X_center, y_center = fun.evaluate(X_center)
@@ -65,11 +65,11 @@ class SIBO:
         
         data['beta'] = fun.beta(data)
         
-        gp = GP(K, ACQ_FUN = ACQ_FUN, SEARCH_METHOD = SEARCH_METHOD)
+        #gp = GP(K, ACQ_FUN = ACQ_FUN, SEARCH_METHOD = SEARCH_METHOD)
         
-        gp.fitting(data, types, iter_fit)
+        #gp.fitting(data, types, iter_fit)
         
-        self.gp = gp
+        #self.gp = gp
         self.D = D
         self.K = K
         self.data = data
@@ -101,37 +101,36 @@ class SIBO:
         
         return next_x
         
-    
-fun = functions.sinc_simple2()
-
-R = SIBO(fun, 1, 5, 2, ACQ_FUN = 'EI', SEARCH_METHOD = 'random', iter_fit = 500)
-
-for i in xrange(10):
-    data = R.data
-    gp = R.gp
-    W = R.W
-    
-    fx = np.random.uniform(-np.sqrt(2),np.sqrt(2), [100, 1])
-    fy = fun.evaluate(np.matmul(W, fx.transpose()).transpose())[1]
-#   fx = np.matmul(fx, fun.W)
-    fxfy = np.concatenate([fx, fy], axis = 1)
-    fxfy = fxfy[fxfy[:, 0].argsort()]
-    
-    mu, var, EI = gp.test(data, R.types, fx)
-    
-    pfxpp = np.concatenate([fx, np.reshape(mu, [-1, 1]), np.reshape(var, [-1, 1]), np.reshape(EI, [-1, 1])], axis = 1)
-    pfxpp = pfxpp[pfxpp[:, 0].argsort()]
-    
-    next_x = R.iterate(500, 10000)
-    
-    plt.figure()
-    plt.plot(fxfy[:, 0], fxfy[:, 1])
-    plt.scatter(data['Z'], data['y'])
-    plt.plot(pfxpp[:, 0], (np.max(fy) - np.min(fy)) * (pfxpp[:, 3]) / (np.max(EI) - np.min(EI)) + min(fy), '-.')
-    plt.plot(pfxpp[:, 0], pfxpp[:, 1], 'k')
-    plt.plot(pfxpp[:, 0], pfxpp[:, 1] + np.sqrt(pfxpp[:, 2]), 'k:')
-    plt.plot(pfxpp[:, 0], pfxpp[:, 1] - np.sqrt(pfxpp[:, 2]), 'k:')
-    plt.scatter(data['Z'][-1], np.min(data['y']), marker = 'x', color = 'g')
-    plt.title('N is ' + str(len(data['y'])))
-    plt.show()
-    
+##fun = functions.sinc_simple2()
+##
+##R = SIBO(fun, 1, 5, 2, ACQ_FUN = 'EI', SEARCH_METHOD = 'random', iter_fit = 500)
+##
+##for i in xrange(10):
+##    data = R.data
+##    gp = R.gp
+##    W = R.W
+##    
+##    fx = np.random.uniform(-np.sqrt(2),np.sqrt(2), [100, 1])
+##    fy = fun.evaluate(np.matmul(W, fx.transpose()).transpose())[1]
+###   fx = np.matmul(fx, fun.W)
+##    fxfy = np.concatenate([fx, fy], axis = 1)
+##    fxfy = fxfy[fxfy[:, 0].argsort()]
+##    
+##    mu, var, EI = gp.test(data, R.types, fx)
+##    
+##    pfxpp = np.concatenate([fx, np.reshape(mu, [-1, 1]), np.reshape(var, [-1, 1]), np.reshape(EI, [-1, 1])], axis = 1)
+##    pfxpp = pfxpp[pfxpp[:, 0].argsort()]
+##    
+##    next_x = R.iterate(500, 10000)
+##    
+##    plt.figure()
+##    plt.plot(fxfy[:, 0], fxfy[:, 1])
+##    plt.scatter(data['Z'], data['y'])
+##    plt.plot(pfxpp[:, 0], (np.max(fy) - np.min(fy)) * (pfxpp[:, 3]) / (np.max(EI) - np.min(EI)) + min(fy), '-.')
+##    plt.plot(pfxpp[:, 0], pfxpp[:, 1], 'k')
+##    plt.plot(pfxpp[:, 0], pfxpp[:, 1] + np.sqrt(pfxpp[:, 2]), 'k:')
+##    plt.plot(pfxpp[:, 0], pfxpp[:, 1] - np.sqrt(pfxpp[:, 2]), 'k:')
+##    plt.scatter(data['Z'][-1], np.min(data['y']), marker = 'x', color = 'g')
+##    plt.title('N is ' + str(len(data['y'])))
+##    plt.show()
+##    
